0342f6f29927369fd15ef391f391302c8619f930
Mohamed Elkashif <mohamed_elkashif@windowslive.com>
2015-12-01 00:59:14 +0200

    not working yet

--- Parser/src/parser/Parser.java
+++ Parser/src/parser/Parser.java
@@ -5,17 +5,100 @@
  */
 package parser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jdk.nashorn.internal.parser.TokenType;
+import static jdk.nashorn.internal.parser.TokenType.EQ;
+
 /**
  *
  * @author Mohamed
  */
 public class Parser {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static String token;
+   static public String stmt_seq()
+    {
+        String t = stmt();
+        String p =t;
+        while((token!="\n") && (token!="ELSE")&&(token!="END")&&(token!="UNTIL"))
+        {
+            String q;
+            match(";");
+            q=stmt();
+//            if(q!=null)
+//            {
+//                if(t==null)
+//                    t=p=q;
+//                else
+//                {
+//                    p=q;
+//                }
+//            }
+        }
+        return t;
     }
+
     
+    static public String match(String x)
+    {
+        return null;
+        
+    }
+   static public String stmt()
+    {
+        return null;
+    }
+   static public String if_stmt()
+    {
+        return null;
+    }
+   static public String repeat_stmt()
+    {
+        return null;
+    }
+   static public String assign_stmt()
+    {
+        return null;
+    }
+   static public String read_stmt()
+    {
+        return null;
+    }
+   static public String write_stmt()
+    {
+        return null;
+    }
+    static public String exp()
+    {
+        return null;
+    }
+   static public String term()
+    {
+        return null;
+    }    
+   static public String factor()
+    {
+        return null;
+    }
+   static public String simple_exp()
+    {
+        return null;
+    }
+    public static void main(String[] args) {
+        
+        //input file
+        //output file
+        String t;
+        token = "read identifier";
+        t=stmt_seq();
+        if(token=="\n")
+        {
+            System.out.println("error");
+        }
+            
+    }
 }
